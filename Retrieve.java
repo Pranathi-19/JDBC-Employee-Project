4e1395c13803dbb84078f3f8a46ad6286e6b9f07
Pranathi-19 <24wh1a0584@bvrithyderabad.edu.in>
2026-02-16 10:29:20 +0530

    Added Retrieve, Update, Delete JDBC files
    Added Java files implementing JDBC operations:
    - Retrieve.java: Fetch employee records from database
    - Update.java: Update employee details
    - Delete.java: Remove employee records from database
    
    These files are part of the JDBC Employee Project.

---
+++ Retrieve.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Retrieve {
+	public static void main(String[] args) throws SQLException {
+		try {
+		   //Step1: register the driver
+		   DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+		
+		   //Step2: Connect the server by supplying URL(ip,port,uname)
+		   Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db584","root","1234");  //"url","db userName","db password"
+		
+		   //Step3: Create Statement object to send queries to db
+		   Statement statement = connection.createStatement();
+		
+		   //Step4: Execute query 
+		   ResultSet rs = statement.executeQuery("select * from employee");
+		   while(rs.next()) {
+			  System.out.println(rs.getString("eId")+"\t" +rs.getString("ename")+"\t" +rs.getString("deptId")+"\t");
+		   }
+		   //Step5: Close the connection
+		   connection.close();
+	    }
+	    catch(SQLException e) {
+		   e.printStackTrace();
+	    }
+	}
+}
